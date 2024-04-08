0145453c5b3a6016a8775f2f4dc71dad423ca2f6
ANA <Usuario@DESKTOP-UTV8S04>
2024-04-08 23:49:21 +0200

    ultimo ejercicio

--- 45_cursos_ficheros/src/principal/GuardarCursosFichero.java
+++ 45_cursos_ficheros/src/principal/GuardarCursosFichero.java
@@ -1,53 +1,51 @@
-package principal;
-
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.PrintStream;
 import java.util.Scanner;
 
 public class GuardarCursosFichero {
 
 	public static void main(String[] args) {
-		String[] cursos = {
-
-				"Java básico", "Python para principaiantes", "Java EE en Web", "JavaScript en Front", "POO con Java",
-				"Python y Django", "Desarrollo Web con ASP.NET", "Aplicaciones Web con Python"
-
-		};
-//solicitamos por teclado la introduccion de un nombre y
-//guardaremos en un fichero todos los cursos que contengan
-//ese nombre.
-		
-		Scanner sc= new Scanner(System.in);
-		System.out.println("Introduce el nombre a buscar");
-		String palabra=sc.nextLine();
-		
-		//Separar por comas
-		
-		
-		static void guardaCursos(String nombre,String []cursos) {
-			String ruta="cursos.txt";
-			try( PrintStream out = new PrintStream(ruta)) {
-				
-				
-			}catch (FileNotFoundException ex) {
-				ex.printStackTrace();
-			}
-
-			catch (IOException ex) {
-				ex.printStackTrace();
-
-			}
-					for(String c:cursos) {
-						
-						
-		}
-		
-		}
+		String [] cursos= {
+				"Java básico",
+				"Python para principaiantes",
+				"Java EE en Web",
+				"JavaScript en Front",
+				"POO con Java",
+				"Python y Django",
+				"Desarrollo Web con ASP.NET",
+				"Aplicaciones Web con Python"
+			};
+			
+			//solicitamos por teclado la introducción de un 
+			//nombre y guardaremos en un fichero todos los
+			//cursos que contengan ese nombre
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Introduzca el nombre");
+		String nombre=sc.nextLine();
+		guardarRuta(nombre,cursos);		
 	}
+
+	
+  static void guardarRuta(String nombre,String[]cursos) {
+	  String ruta="nombre.txt";
+	  try {
+		PrintStream out = new PrintStream(ruta);  //Lo primero es crearme un objeto
+		for(String c:cursos) {
+			if(c.contains(nombre)) {
+				out.print(c+"\n");
+			}
+		}
+	} catch (FileNotFoundException e) {
+		
+		e.printStackTrace();
+	}
+
+	
+	
+	
+	
 }
-		
-		//String ruta="cursos.txt";
-	}
+	
+	
 
 }
